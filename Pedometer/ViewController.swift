bfc1f6d6946e1a959770ec03da7b118b12d1c32f
kalpana jangir <kalpana.jangir@farintuit.com>
2019-05-23 23:40:09 +0530

    Initial Commit

---
+++ Pedometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pedometer
+//
+//  Created by kalpana on 23/05/19.
+//  Copyright © 2019 kalpana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
